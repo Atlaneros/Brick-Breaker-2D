39629fc4d6b03bab9f848f50bda438ea0a1cc394
Atlaneros <32978938+Atlaneros@users.noreply.github.com>
2021-07-28 18:05:43 +0200

    Full project upload
    I upload my full finished Brick Breaker game.

---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+
+    // configuration paramaters
+    [SerializeField] float minX = 1f;
+    [SerializeField] float maxX = 15f;
+    [SerializeField] float screenWidthInUnits = 16f;
+    // Use this for initialization
+
+
+    GameSession theGameSession;
+    Ball theBall;
+    void Start()
+    {
+        theGameSession = FindObjectOfType<GameSession>();
+        theBall = FindObjectOfType<Ball>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mousePosInUnits = Input.mousePosition.x / Screen.width * screenWidthInUnits;
+        Vector2 paddlePos = new Vector2(transform.position.x, transform.position.y);
+        paddlePos.x = Mathf.Clamp(GetXPos(), minX, maxX);
+        transform.position = paddlePos;
+    }
+
+    private float GetXPos()
+    {
+        if (theGameSession.IsAutoPlayEnabled()) //stavili smo theGameSession i 
+        { //i theBall da pozivamo iz starta jer smo ih odmah pronasili i ne moramo ih vise traziti 
+            //jer pri svakom frejmu je to skupa operacija
+            return theBall.transform.position.x;
+        }
+        else
+        {
+            return Input.mousePosition.x / Screen.width * screenWidthInUnits;
+        }
+    }
+}
